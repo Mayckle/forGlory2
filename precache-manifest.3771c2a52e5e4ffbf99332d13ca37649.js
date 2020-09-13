435aded97ae615c9a2bdb43691cbdb17b49b2d5a
Mayckle <68104231+Mayckle@users.noreply.github.com>
2020-09-13 22:03:07 +0200

    Updates

---
+++ precache-manifest.3771c2a52e5e4ffbf99332d13ca37649.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02def07a34baa9b79980a5155d150260",
+    "url": "/forGlory2/index.html"
+  },
+  {
+    "revision": "200d4b8097b957c19a3c",
+    "url": "/forGlory2/static/css/main.e97ca201.chunk.css"
+  },
+  {
+    "revision": "54453c31a534e7dcbcb2",
+    "url": "/forGlory2/static/js/2.a7601f91.chunk.js"
+  },
+  {
+    "revision": "a1a636632721a402612df3e043dd630d",
+    "url": "/forGlory2/static/js/2.a7601f91.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "200d4b8097b957c19a3c",
+    "url": "/forGlory2/static/js/main.45834c5b.chunk.js"
+  },
+  {
+    "revision": "b6ab33ee6dc697bc4fea",
+    "url": "/forGlory2/static/js/runtime-main.36e26e63.js"
+  },
+  {
+    "revision": "fcf8ffdb75018caed5c5fb409125db9c",
+    "url": "/forGlory2/static/media/logo.fcf8ffdb.png"
+  },
+  {
+    "revision": "d64f721d2f82210fd42b95fe2745a9fd",
+    "url": "/forGlory2/static/media/sam.d64f721d.png"
+  },
+  {
+    "revision": "a9d06a6f2d49563b12a06ed5bd4a827d",
+    "url": "/forGlory2/static/media/vik.a9d06a6f.png"
+  }
+]);
